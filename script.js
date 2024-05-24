fae905d2935b2c607a186267be1adede7ab2bc5f
Ariom Pimenta <ariompf@gmail.com>
2024-05-24 07:19:53 -0300

    add remove/add.classList active

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //Elementos do HTML
-const html = document.querySelector('html')
+const html = document.querySelector('html');
 const displayTempo = document.querySelector('#timer');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 
 //Botões de Seleção da Atividade
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll('.app__card-button');
 
 //Botão do Timer
 const iniciarBt = document.querySelector('.app__card-primary-button');
@@ -22,17 +23,23 @@ const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 });
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 });
 
 function alterarContexto(contexto) {
+    botoes.forEach(function(contexto) {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
